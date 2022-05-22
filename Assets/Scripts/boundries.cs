f400cfe53e0f42207a9811ad0ea1c9c18b955c35
Echo224JJ <jokejung@ucsc.edu>
2022-05-21 19:50:41 -0700

    Bounds Script
    Bounds script added but disabled cuz its kinda broken

--- Assets/Scripts/boundries.cs
+++ Assets/Scripts/boundries.cs
@@ -11,12 +11,16 @@ public class boundries : MonoBehaviour
     void Start()
     {
         screenBounds= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 viewPos = transform.position;
-        //viewPos.x = Mathf.Clamp(viewPos.x)
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight);
+        transform.position = viewPos;
     }
 }
